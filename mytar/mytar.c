2ce8dd0fb78976505deb73dbcca96835b43e232f
Vishal Ramesh <vishal.ramesh@hotmail.com>
2020-06-14 23:16:57 +0800

    Zero block

--- mytar/mytar.c
+++ mytar/mytar.c
@@ -112,11 +112,11 @@ int main(int argc, char *argv[]) {
             }
 
             if (d == EOF) {
-                if (start != 0) {
-                    fprintf(stderr, "mytar: Unexpected EOF in archive\n");
-                    fprintf(stderr, "mytar: Error is not recoverable: exiting now\n");
-                    return (2);
-                }
+                // if (start != 0) {
+                //     fprintf(stderr, "mytar: Unexpected EOF in archive\n");
+                //     fprintf(stderr, "mytar: Error is not recoverable: exiting now\n");
+                //     return (2);
+                // }
                 break;
             }
 
