dac69823255df7ad847fadd6edcefd3e17403ab0
Vishal Ramesh <vishal.ramesh@hotmail.com>
2020-06-15 01:36:34 +0800

    half only

--- mytar/mytar.c
+++ mytar/mytar.c
@@ -139,11 +139,11 @@ int main(int argc, char *argv[]) {
             }
 
             if (d == EOF) {
-                // if (start != 0) {
-                //     fprintf(stderr, "mytar: Unexpected EOF in archive\n");
-                //     fprintf(stderr, "mytar: Error is not recoverable: exiting now\n");
-                //     return (2);
-                // }
+                if (start != 0) {
+                    fprintf(stderr, "mytar: Unexpected EOF in archive\n");
+                    fprintf(stderr, "mytar: Error is not recoverable: exiting now\n");
+                    return (2);
+                }
                 // if ((char_count % 512) != 0) {
                 //     fflush(stdout);
                 //     fprintf(stderr, "mytar: Unexpected EOF in archive\n");
