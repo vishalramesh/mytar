f9437b6aaf6941502f73fcc08ab7ea0a0967d5f4
Vishal Ramesh <vishal.ramesh@hotmail.com>
2020-06-17 20:26:02 +0800

    argparse

--- mytar/mytar.c
+++ mytar/mytar.c
@@ -2,7 +2,7 @@
 #include <ctype.h>
 #include <string.h>
 
-int arg_parse(int argc, char *argv[], int *list_argument, int *tarfile_argument, FILE** file);
+int arg_parse(int argc, char *argv[], int *list_arg_index, int *file_arg_index, FILE** file);
 
 int ascii_to_decimal(char size[], int len);
 int roundup_to_multiple(int decimal, int multiple);
@@ -16,232 +16,180 @@ int is_suffix(char argv[], char name[]);
 
 int main(int argc, char *argv[]) {
 
-    int list_argument = 0;
-    int tarfile_argument = 0;
+    int list_arg_index = 0;
+    int file_arg_index = 0;
     FILE* file = NULL;
 
-    arg_parse(argc, argv, &list_argument, &tarfile_argument, &file);
+    arg_parse(argc, argv, &list_arg_index, &file_arg_index, &file);
 
-    // if (!(argc >= 2)) {
-    //     fflush(stdout);
-    //     fprintf(stderr, "mytar: need at least one option\n");
-    //     return 2;
+
+    int list_arg_present;
+    if (list_arg_index >= argc) {
+        list_arg_present = 0;
+    } else if (strcmp(argv[list_arg_index], "-f") == 0 && file_arg_index == argc - 1) {
+        list_arg_present = 0;
+    } else {
+        list_arg_present = 1;
+    }
+
+    // if (strcmp(argv[list_arg_index], "-f") == 0 && file_arg_index < argc - 1) {
+    //     list_arg_index = file_arg_index + 1;
     // }
-    // int list_argument;
-    // int tarfile_argument;
-    // int file_arg_present = 0;
-    // for (int i = 1; i < argc; i++) {
-    //     switch (argv[i][0]) {
-    //     case '-':
-    //         switch (argv[i][1]) {
-    //         case 'f':
-    //             if (i == argc - 1) {
-    //                 fflush(stdout);
-    //                 fprintf(stderr, "mytar: option requires an argument -- %s\n", argv[i]);
-    //                 return 64;
-    //             }
-    //             if ((i + 1) < argc && (strcmp(argv[i + 1], "-t") == 0)) {
-    //                 fflush(stdout);
-    //                 fprintf(stderr, "mytar: You must specify one of the options\n");
-    //                 return 2;
-    //             }
-    //             tarfile_argument = i + 1;
-    //             file_arg_present = 1;
-    //             break;
-    //         case 't':
-    //             list_argument = i + 1;
-    //             break;
-    //         default:
-    //             fflush(stdout);
-    //             fprintf(stderr, "mytar: Unknown option: %s\n", argv[i]);
-    //             return 2;
-    //             break;
-    //         }
-    //         break;
-    //     default:
-    //         break;
-    //     }
-    // }
-    // if (file_arg_present == 0) {
-    //     fflush(stdout);
-    //     fprintf(stderr, "mytar: Refusing to read archive contents from terminal (missing -f option?)\n");
-    //     fflush(stdout);
-    //     fprintf(stderr, "mytar: Error is not recoverable: exiting now\n");
-    //     return (2);
-    // }
-    // FILE *file = fopen(argv[tarfile_argument], "r");
-    // if (file == NULL) {
-    //     fflush(stdout);
-    //     fprintf(stderr, "mytar: %s: Cannot open: No such file or directory\n", argv[tarfile_argument]);
-    //     fflush(stdout);
-    //     fprintf(stderr, "mytar: Error is not recoverable: exiting now\n");
-    //     return 2;
-    // } else {
-        int list_arg_present;
-        if (list_argument >= argc) {
-            list_arg_present = 0;
-        } else if (strcmp(argv[list_argument], "-f") == 0 && tarfile_argument == argc - 1) {
-            list_arg_present = 0;
-        } else {
-            list_arg_present = 1;
+    
+    int final_list_arg_index = list_arg_index;
+    while (final_list_arg_index + 1 < argc) {
+        if (strcmp(argv[final_list_arg_index + 1], "-f") == 0) {
+            break;
         }
-        if (strcmp(argv[list_argument], "-f") == 0 && tarfile_argument < argc - 1) {
-            list_argument = tarfile_argument + 1;
+        final_list_arg_index += 1;
+    }
+    int print_file[final_list_arg_index - list_arg_index + 1];
+    for (int i = 0; i < final_list_arg_index - list_arg_index + 1; i++) {
+        print_file[i] = 0;
+    }
+
+    int offset = 0;
+
+    int no_zero = 0;
+
+    int char_count = 0;
+
+    int first_time = 0;
+
+    int d;
+    char header[512];
+    char name[100];
+    char size[12];
+    char typeflag;
+    int start = 0;
+    int block_no = 0;
+    while (1) {
+        if (file == NULL) {
+            break;
         }
-        int final_list_argument = list_argument;
-        while (final_list_argument + 1 < argc) {
-            if (strcmp(argv[final_list_argument + 1], "-f") == 0) {
-                break;
-            }
-            final_list_argument += 1;
+        start = 0;
+        while (start < 512 && (d = fgetc(file)) != EOF) {
+            header[start] = d;
+            start += 1;
+            char_count += 1;
         }
-        int print_file[final_list_argument - list_argument + 1];
-        for (int i = 0; i < final_list_argument - list_argument + 1; i++) {
-            print_file[i] = 0;
-        }
+        block_no += 1;
 
-        int offset = 0;
-
-        int no_zero = 0;
-
-        int char_count = 0;
-
-        int first_time = 0;
-
-        int d;
-        char header[512];
-        char name[100];
-        char size[12];
-        char typeflag;
-        int start = 0;
-        int block_no = 0;
-        while (1) {
-            if (file == NULL) {
-                break;
-            }
-            start = 0;
-            while (start < 512 && (d = fgetc(file)) != EOF) {
-                header[start] = d;
-                start += 1;
-                char_count += 1;
-            }
-            block_no += 1;
-
+        
+        if (is_zero_block(header)) {
             
-            if (is_zero_block(header)) {
-                
-                FILE *p = file;
-                fseek(p, 512, SEEK_SET);
-                if (!first_time) {
-                    first_time = 1;
-                    for (int i = 0; i < 512; ++i) {
-                        if ((d = fgetc(p)) != '\0' && block_no != 10) {
-                            printf("mytar: A lone zero block at 22\n");//, block_no);
-                            break;
-                        }
+            FILE *p = file;
+            fseek(p, 512, SEEK_SET);
+            if (!first_time) {
+                first_time = 1;
+                for (int i = 0; i < 512; ++i) {
+                    if ((d = fgetc(p)) != '\0' && block_no != 10) {
+                        printf("mytar: A lone zero block at 22\n");//, block_no);
+                        break;
                     }
                 }
             }
+        }
 
-            if (d == EOF) {
-                if (start != 0) {
-                    fflush(stdout);
-                    fprintf(stderr, "mytar: Unexpected EOF in archive\n");
-                    fflush(stdout);
-                    fprintf(stderr, "mytar: Error is not recoverable: exiting now\n");
-                    return 2;
-                }
-                if ((char_count % 512) != 0) {
-                    fflush(stdout);
-                    fprintf(stderr, "mytar: Unexpected EOF in archive\n");
-                    fflush(stdout);
-                    fprintf(stderr, "mytar: Error is not recoverable: exiting now\n");
-                    return 2;
-                }
-                
-                break;
-            }
-
-            if (is_zero_block(header)) {
-                no_zero += 1;
-               
-            }
-
-            for (int i = 0; i < 100; ++i) {
-                name[i] = header[i];
-            }
-            for (int i = 124; i < 136; ++i) {
-                size[i - 124] = header[i];
-            }
-            typeflag = header[156];
-
-            
-            if (typeflag != '0' && typeflag != '\0') {
+        if (d == EOF) {
+            if (start != 0) {
                 fflush(stdout);
-                fprintf(stderr, "mytar: Unsupported header type: %d\n", typeflag);
+                fprintf(stderr, "mytar: Unexpected EOF in archive\n");
+                fflush(stdout);
+                fprintf(stderr, "mytar: Error is not recoverable: exiting now\n");
                 return 2;
             }
-            if (!list_arg_present) {
-                int i = 0;
-                int printable = 0;
-                while (name[i] != '\0') {
-                    if (isalnum(name[i])) {
-                        printable = 1;
-                    }
-                    i += 1;
-                }
-                if (printable) {
-                    printf("%s\n", name);
-                    fflush(stdout);
-                }
-            }
-            if (list_arg_present) {
-                for (int q = list_argument; q <= final_list_argument; q++) {
-                    if (is_equal(argv[q], name) || is_prefix(argv[q], name) || is_suffix(argv[q], name)) {
-                        int i = 0;
-                        int printable = 0;
-                        while (name[i] != '\0') {
-                            if (isalnum(name[i])) {
-                                printable = 1;
-                            }
-                            i += 1;
-                        }
-                        if (printable) {
-                            printf("%s\n", name);
-                            fflush(stdout);
-                        }
-                        print_file[q - list_argument] = 1; 
-                    }
-                }
+            if ((char_count % 512) != 0) {
+                fflush(stdout);
+                fprintf(stderr, "mytar: Unexpected EOF in archive\n");
+                fflush(stdout);
+                fprintf(stderr, "mytar: Error is not recoverable: exiting now\n");
+                return 2;
             }
             
-            int size_len = sizeof(size) / sizeof(size[0]);
-            offset += 512;
-            offset += roundup_to_multiple(ascii_to_decimal(size, size_len), 512);
-            
-            fseek(file, offset, SEEK_SET);
-  
+            break;
         }
 
+        if (is_zero_block(header)) {
+            no_zero += 1;
+            
+        }
+
+        for (int i = 0; i < 100; ++i) {
+            name[i] = header[i];
+        }
+        for (int i = 124; i < 136; ++i) {
+            size[i - 124] = header[i];
+        }
+        typeflag = header[156];
+
+        
+        if (typeflag != '0' && typeflag != '\0') {
+            fflush(stdout);
+            fprintf(stderr, "mytar: Unsupported header type: %d\n", typeflag);
+            return 2;
+        }
+        if (!list_arg_present) {
+            int i = 0;
+            int printable = 0;
+            while (name[i] != '\0') {
+                if (isalnum(name[i])) {
+                    printable = 1;
+                }
+                i += 1;
+            }
+            if (printable) {
+                printf("%s\n", name);
+                fflush(stdout);
+            }
+        }
         if (list_arg_present) {
-            int fail = 0;
-            for (int i = list_argument; i <= final_list_argument; i++) {
-                if (!print_file[i - list_argument]) {
-                    fflush(stdout);
-                    fprintf(stderr, "mytar: %s: Not found in archive\n", argv[i]);
-                    fail = 1;
+            for (int q = list_arg_index; q <= final_list_arg_index; q++) {
+                if (is_equal(argv[q], name) || is_prefix(argv[q], name) || is_suffix(argv[q], name)) {
+                    int i = 0;
+                    int printable = 0;
+                    while (name[i] != '\0') {
+                        if (isalnum(name[i])) {
+                            printable = 1;
+                        }
+                        i += 1;
+                    }
+                    if (printable) {
+                        printf("%s\n", name);
+                        fflush(stdout);
+                    }
+                    print_file[q - list_arg_index] = 1; 
                 }
             }
-            if (fail) {
+        }
+        
+        int size_len = sizeof(size) / sizeof(size[0]);
+        offset += 512;
+        offset += roundup_to_multiple(ascii_to_decimal(size, size_len), 512);
+        
+        fseek(file, offset, SEEK_SET);
+
+    }
+
+    if (list_arg_present) {
+        int fail = 0;
+        for (int i = list_arg_index; i <= final_list_arg_index; i++) {
+            if (!print_file[i - list_arg_index]) {
                 fflush(stdout);
-                fprintf(stderr, "mytar: Exiting with failure status due to previous errors\n");
-                return 2;
+                fprintf(stderr, "mytar: %s: Not found in archive\n", argv[i]);
+                fail = 1;
             }
         }
+        if (fail) {
+            fflush(stdout);
+            fprintf(stderr, "mytar: Exiting with failure status due to previous errors\n");
+            return 2;
+        }
+    }
     // }
 }
 
-int arg_parse(int argc, char *argv[], int *list_argument, int *tarfile_argument, FILE **file) {
+int arg_parse(int argc, char *argv[], int *list_arg_index, int *file_arg_index, FILE **file) {
     if (!(argc >= 2)) {
         fflush(stdout);
         fprintf(stderr, "mytar: need at least one option\n");
@@ -261,16 +209,16 @@ int arg_parse(int argc, char *argv[], int *list_argument, int *tarfile_argument,
                     fprintf(stderr, "mytar: option requires an argument -- -%c\n", char_1);
                     return 64;
                 }
-                if ((i + 1) < argc && (strcmp(argv[i + 1], "-t") == 0)) {
+                if (i < argc - 1 && (strcmp(argv[i + 1], "-t") == 0)) {
                     fflush(stdout);
                     fprintf(stderr, "mytar: You must specify one of the options\n");
                     return 2;
                 }
                 file_arg_present = 1;
-                *tarfile_argument = i + 1;
+                *file_arg_index = i + 1;
                 break;
             case 't':
-                *list_argument = i + 1;
+                *list_arg_index = i + 1;
                 break;
             default:
                 fflush(stdout);
@@ -278,6 +226,10 @@ int arg_parse(int argc, char *argv[], int *list_argument, int *tarfile_argument,
                 return 2;
             }
         }   
+    }
+    
+    if (strcmp(argv[*list_arg_index], "-f") == 0 && *file_arg_index < argc - 1) {
+        *list_arg_index = *file_arg_index + 1;
     }
 
     if (!file_arg_present) {
@@ -288,10 +240,10 @@ int arg_parse(int argc, char *argv[], int *list_argument, int *tarfile_argument,
         return 2;
     }
 
-    *file = fopen(argv[*tarfile_argument], "r");
+    *file = fopen(argv[*file_arg_index], "r");
     if (*file == NULL) {
         fflush(stdout);
-        fprintf(stderr, "mytar: %s: Cannot open: No such file or directory\n", argv[*tarfile_argument]);
+        fprintf(stderr, "mytar: %s: Cannot open: No such file or directory\n", argv[*file_arg_index]);
         fflush(stdout);
         fprintf(stderr, "mytar: Error is not recoverable: exiting now\n");
         return 2;
