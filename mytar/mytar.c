ee6e7d0274288e208c1869bad723edefc55d167a
Vishal Ramesh <vishal.ramesh@hotmail.com>
2020-06-17 17:00:55 +0800

    Refactoring equal and iszeroblock

--- mytar/mytar.c
+++ mytar/mytar.c
@@ -343,37 +343,23 @@ int power(int base, int exp) {
 }
 
 int equal(char arg_file_name[], char file_name[]) {
-    
-    for (int q = 0; arg_file_name[q] != '\0' && file_name[q] != '\0'; q++) {
+    int i = 0;
+    while (arg_file_name[i] != '\0') {
+        i += 1;
+    }
+    int j = 0;
+    while (file_name[j] != '\0') {
+        j += 1;
+    }
+    if (i != j) {
+        return 0;
+    }
+    for (int q = 0; q < i; q++) {
         if (arg_file_name[q] != file_name[q]) {
             return 0;
         }
     }
     return 1;
-    // while (arg_file_name[q] != file_name[q] || (int) *arg_file_name[q] == NULL) {
-    //     if (arg_file_name[q] == '\0') {
-    //         return 1;
-    //     }
-    //     q += 1;
-    // }
-    // return 0;
-    // int i = 0;
-    // while (arg_file_name[i] != '\0') {
-    //     i += 1;
-    // }
-    // int j = 0;
-    // while (file_name[j] != '\0') {
-    //     j += 1;
-    // }
-    // if (i != j) {
-    //     return 0;
-    // }
-    // for (int q = 0; q < i; q++) {
-    //     if (arg_file_name[q] != file_name[q]) {
-    //         return 0;
-    //     }
-    // }
-    // return 1;
 }
 
 int iszeroblock(char header[]) {
