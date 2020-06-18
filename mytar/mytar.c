5a0fcfeb3095f7cb28d867d96f23b0310afd54ce
Vishal Ramesh <vishal.ramesh@hotmail.com>
2020-06-18 19:02:31 +0800

    fix continue c

--- mytar/mytar.c
+++ mytar/mytar.c
@@ -172,6 +172,7 @@ int main(int argc, char *argv[]) {
 
         if (extract_arg_present && args_present[3] && !args_present[2]) { // Without -v
 
+            int c = 0;
             for (int q = extract_arg_index; q <= final_extract_arg_index; q++) {
                 if (is_equal(argv[q], file_name) || is_prefix(argv[q], file_name) || is_suffix(argv[q], file_name)) {
                     FILE* create_file = fopen(file_name, "w");
@@ -181,14 +182,19 @@ int main(int argc, char *argv[]) {
                     if (write_ret != 0) {
                         return write_ret;
                     }
-                    continue;
+                    c = 1;
+                    break;
                 }
             }
-
+            if (c) {
+                continue;
+            }
+            
         }
 
         if (extract_arg_present && args_present[3] && args_present[2]) {
 
+            int c = 0;
             for (int q = extract_arg_index; q <= final_extract_arg_index; q++) {
                 if (is_equal(argv[q], file_name) || is_prefix(argv[q], file_name) || is_suffix(argv[q], file_name)) {
                     printf("%s\n", file_name);
@@ -199,9 +205,13 @@ int main(int argc, char *argv[]) {
                     if (write_ret != 0) {
                         return write_ret;
                     }
-                    continue;
+                    c = 1;
+                    break;
                 }
-            }   
+            }
+            if (c) {
+                continue;
+            }
 
         }
 
