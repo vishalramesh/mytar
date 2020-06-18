96d72703bd7d752c5ae7b186dfe564038ef84ce2
Vishal Ramesh <vishal.ramesh@hotmail.com>
2020-06-18 14:11:27 +0800

    14

--- mytar/mytar.c
+++ mytar/mytar.c
@@ -158,7 +158,7 @@ int advance_offset_and_block(char size[], int *offset, int *block_no, FILE* file
     *offset += roundup_to_multiple(ascii_to_decimal(size, size_len), 512);
     *block_no += (roundup_to_multiple(ascii_to_decimal(size, size_len), 512) / 512);
     FILE *p = file;
-    fseek(p, 512, SEEK_CUR);
+    fseek(p, 0, SEEK_CUR);
     
     for (int i = 0; i < ascii_to_decimal(size, size_len); ++i) {
         int d;
