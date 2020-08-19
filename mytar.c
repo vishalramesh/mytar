149c8e5aad3a2a32ab07de1c66c24d9ea551501d
Vishal Ramesh <vishal.ramesh@hotmail.com>
2020-08-19 16:49:18 +0200

    Refactor

--- mytar.c
+++ mytar.c
@@ -377,8 +377,7 @@ int main(int argc, char *argv[]) {
         }
         typeflag = header[156];
 
-        //if (magic[0] != 'u' || magic[1] != 's' || magic[2] != 't' || magic[3] != 'a' || magic[4] != 'r') {
-        if (strcmp(magic, "ustar\0") != 0) {
+        if (magic[0] != 'u' || magic[1] != 's' || magic[2] != 't' || magic[3] != 'a' || magic[4] != 'r') {
             fflush(stdout);
             fprintf(stderr, "mytar: This does not look like a tar archive\n");
             fprintf(stderr, "mytar: Exiting with failure status due to previous errors\n");
