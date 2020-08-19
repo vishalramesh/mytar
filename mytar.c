979cf8de9d4aae7cd42423ddffaef38710bcb8ab
Vishal Ramesh <vishal.ramesh@hotmail.com>
2020-08-19 14:05:31 +0200

    Experiment

--- mytar.c
+++ mytar.c
@@ -295,7 +295,6 @@ int write_to_file(FILE* file, FILE* create_file, int *offset, int *block_no, cha
         if ((d = fgetc(p)) == EOF) {
             fflush(stdout);
             fprintf(stderr, "mytar: Unexpected EOF in archive\n");
-            fflush(stdout);
             fprintf(stderr, "mytar: Error is not recoverable: exiting now\n");
             return 2;
         }
@@ -308,18 +307,89 @@ int write_to_file(FILE* file, FILE* create_file, int *offset, int *block_no, cha
 
 int main(int argc, char *argv[]) {
 
-    int list_arg_index = 0;
+    // int list_arg_index = 0;
+    // int file_arg_index = 0;
+    // int extract_arg_index = 0;
+
+    // int args_present[4] = {0, 0, 0, 0};
+    
+    // FILE* file = NULL;
+
+    // int arg_parse_ret;
+    // if ((arg_parse_ret = arg_parse(argc, argv, &list_arg_index, &file_arg_index, &extract_arg_index, args_present, &file)) != 0) {
+    //     return arg_parse_ret;
+    // }
+
+        int args_present[4] = {0, 0, 0, 0};
+
     int file_arg_index = 0;
+    int list_arg_index = 0;
     int extract_arg_index = 0;
 
-    int args_present[4] = {0, 0, 0, 0};
-    
-    FILE* file = NULL;
+    int list_arg_present = 0;
 
-    int arg_parse_ret;
-    if ((arg_parse_ret = arg_parse(argc, argv, &list_arg_index, &file_arg_index, &extract_arg_index, args_present, &file)) != 0) {
-        return arg_parse_ret;
+    if (!(argc >= 2)) {
+        fflush(stdout); 
+        fprintf(stderr, "mytar: need at least one option\n");
+        return 2;
     }
+
+    for (int i = 1; i < argc; ++i) {
+
+        char ch = argv[i][1]; 
+
+        if (argv[i][0] != '-') {
+            continue;
+        } else if (ch == 'f') {
+            if (i >= argc - 1) {
+                fflush(stdout);
+                fprintf(stderr, "mytar: option requires an argument -- -%c\n", ch);
+                return 64;
+            }
+            if (i < argc - 1 && (strcmp(argv[i + 1][0], '-') == 0)) {
+                fflush(stdout);
+                fprintf(stderr, "mytar: You must specify one of the options\n");
+                return 2;
+            }
+            file_arg_index = i + 1;
+            args_present[0] = 1;
+        } else if (ch == 't') {
+            list_arg_index = i + 1;
+            args_present[1] = 1;            
+        } else if (ch == 'v') {
+            args_present[2] = 1;  
+        } else if (ch == 'x') {
+            extract_arg_index = i + 1;
+            args_present[3] = 1;
+        } else {
+            fflush(stdout);
+            fprintf(stderr, "mytar: Unknown option: %c\n", ch);
+            return 2;
+        }
+    }
+
+    if (!args_present[0]) {
+        fflush(stdout);
+        fprintf(stderr, "mytar: Refusing to read archive contents from terminal (missing-f option?)\n");
+        fprintf(stderr, "mytar: Error is not recoverable: exiting now\n");
+        return 2;
+    }
+
+    if (args_present[1] && args_present[3]) {
+        fflush(stdout);
+        fprintf(stderr, "mytar: You may not specify more than one option\n");
+        return 2;
+    }
+
+    FILE *tar_file = fopen(argv[file_arg_index], "r");
+    if (tar_file == NULL) {
+        fflush(stdout);
+        fprintf(stderr, "mytar: %s: Cannot open: No such file or directory\n", argv[file_arg_index]);
+        fprintf(stderr, "mytar: Error is not recoverable: exiting now\n");
+        return 2;
+    }
+
+
     
     int list_arg_present = check_list_arg_present(argc, argv, list_arg_index, file_arg_index) && args_present[1];
     int extract_arg_present = check_list_arg_present(argc, argv, extract_arg_index, file_arg_index) && args_present[3];
