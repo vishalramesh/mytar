52212212402146dfa79754d4b2389e7ae7f99cfe
Vishal Ramesh <vishal.ramesh@hotmail.com>
2020-08-19 13:06:56 +0200

    Refactor

--- mytar.c
+++ mytar.c
@@ -3,244 +3,6 @@
 #include <stdlib.h>
 #include <string.h>
 
-int main(int argc, char *argv[]) {
-
-    int list_arg_index = 0;
-    int file_arg_index = 0;
-    int extract_arg_index = 0;
-
-    int args_present[4] = {0, 0, 0, 0}; // Alphabetical order f -> t -> v -> x
-    
-    FILE* file = NULL;
-
-    int arg_parse_ret;
-    if ((arg_parse_ret = arg_parse(argc, argv, &list_arg_index, &file_arg_index, &extract_arg_index, args_present, &file)) != 0) {
-        return arg_parse_ret;
-    }
-    
-    int list_arg_present = check_list_arg_present(argc, argv, list_arg_index, file_arg_index) && args_present[1];
-    int extract_arg_present = check_list_arg_present(argc, argv, extract_arg_index, file_arg_index) && args_present[3];
-   
-    int final_list_arg_index = get_final_arg_index(argc, argv, list_arg_index);
-    int final_extract_arg_index = get_final_arg_index(argc, argv, extract_arg_index);
-
-    int pf_size = final_list_arg_index - list_arg_index + 1;
-    int print_file[pf_size];
-    initialise_with_zeros(print_file, pf_size);
-
-    int cr_size = final_extract_arg_index - extract_arg_index + 1;
-    int create_file[cr_size];
-    initialise_with_zeros(create_file, cr_size);
-
-    int offset = 0;
-    int block_no = 0;
-
-    int d;
-
-    char header[512];
-    char file_name[100];
-    char size[12];
-    char magic[6];
-    char typeflag;
-
-    while (file != NULL) {
-
-        // printf("shhs");
-
-        int pos = 0;
-        d = get_block(header, file, &pos);
-        block_no += 1;
-	// printf("%s\n", header);
-	// fflush(stdout);
-        
-        if (is_zero_block(header)) {
-            
-            FILE *p = file;
-            
-            for (int i = 0; i < 512; ++i) {
-                // Check partial block here?
-                int e;
-                if ((e = fgetc(p)) != '\0') {
-                    
-                    int this_ret = 0;
-                    if (list_arg_present) {
-                        this_ret = print_list_arg_error(argv, print_file, list_arg_index, final_list_arg_index);
-                    }
-
-                    printf("mytar: A lone zero block at %d\n", block_no); // ???
-		    fflush(stdout);
-                    return this_ret;
-                }
-            }
-
-            if (list_arg_present) {
-                return print_list_arg_error(argv, print_file, list_arg_index, final_list_arg_index);
-            }
-
-            return 0;
-        }
-
-        if (d == EOF) {
-            if (pos != 0) {
-                fflush(stdout);
-                fprintf(stderr, "mytar: Unexpected EOF in archive\n");
-                fflush(stdout);
-                fprintf(stderr, "mytar: Error is not recoverable: exiting now\n");
-                return 2;
-            }
-            if (pos == 0) {
-                return 0;
-            }
-        }
-        
-        for (int i = 0; i < 100; ++i) {
-            file_name[i] = header[i];
-        }
-        for (int i = 124; i < 136; ++i) {
-            size[i - 124] = header[i];
-        }
-        for (int i = 257; i < 263; ++i) {
-            magic[i - 257] = header[i];
-        }
-        typeflag = header[156];
-
-        if (magic[0] != 'u' || magic[1] != 's' || magic[2] != 't' || magic[3] != 'a' || magic[4] != 'r') {
-            fflush(stdout);
-            fprintf(stderr, "mytar: This does not look like a tar archive\n");
-            fflush(stdout);
-            fprintf(stderr, "mytar: Exiting with failure status due to previous errors\n");
-            return 2;
-        }
-
-        if (args_present[1] && typeflag != '0' && typeflag != '\0') {
-            fflush(stdout);
-            fprintf(stderr, "mytar: Unsupported header type: %d\n", typeflag);
-            return 2;
-        }
-
-        if (!list_arg_present && args_present[1]) {
-            print_default_list_output(file_name);
-        }
-
-        if (list_arg_present) {
-            print_list_arg_output(argv, print_file, file_name, list_arg_index, final_list_arg_index);
-        }
-
-        //fprintf(stderr, "%d\n", extract_arg_present);
-        //fprintf(stderr, "%d\n", args_present[3]);
-        //fprintf(stderr, "%d\n", args_present[2]);
-
-        if (!extract_arg_present && args_present[3] && !args_present[2]) { // Without -v
-            FILE* create_file = fopen(file_name, "w");
-            int write_ret = write_to_file(file, create_file, &offset, &block_no, size);
-            fclose(create_file);
-            if (write_ret != 0) {
-                return write_ret;
-            }
-            continue;
-        }
-        
-        // printf("%d\n", args_present[3]);
-        // printf("%d\n", args_present[2]);
-        if (!extract_arg_present && args_present[3] && args_present[2]) { // With -v
-            printf("%s\n", file_name);
-	    fflush(stdout);
-            //printf("shjhs\n");
-            FILE* create_file = fopen(file_name, "w");
-            int write_ret = write_to_file(file, create_file, &offset, &block_no, size);
-	    // printf("shhs\n");
-	    //fflush(stdout);
-            // fclose(create_file);
-	    // printf("%d\n", write_ret);
-	    // fflush(stdout);
-            if (write_ret != 0) {
-	    	// fflush(stdout);
-	    	// fprintf(stderr, "shhs\n");
-		//fflush(stdout);
-                return write_ret;
-            }
-	    //fflush(stdout);
-	    //fprintf(stderr, "shhs\n");
-	    
-            //printf("shs\n");
-            continue;
-        }
-
-        if (extract_arg_present && args_present[3] && !args_present[2]) { // Without -v
-
-            int c = 0;
-            for (int q = extract_arg_index; q <= final_extract_arg_index; q++) {
-                if (is_equal(argv[q], file_name) || is_prefix(argv[q], file_name) || is_suffix(argv[q], file_name)) {
-                    FILE* create_file = fopen(file_name, "w");
-                    int write_ret = write_to_file(file, create_file, &offset, &block_no, size);
-                    fclose(create_file);
-                    print_file[q - list_arg_index] = 1; 
-                    if (write_ret != 0) {
-                        return write_ret;
-                    }
-                    c = 1;
-                    break;
-                }
-            }
-            if (c) {
-                continue;
-            }
-            
-        }
-
-        if (extract_arg_present && args_present[3] && args_present[2]) {
-
-            int c = 0;
-            for (int q = extract_arg_index; q <= final_extract_arg_index; q++) {
-                if (is_equal(argv[q], file_name) || is_prefix(argv[q], file_name) || is_suffix(argv[q], file_name)) {
-                    printf("%s\n", file_name);
-                    FILE* create_file = fopen(file_name, "w");
-                    int write_ret = write_to_file(file, create_file, &offset, &block_no, size);
-                    fclose(create_file);
-                    print_file[q - list_arg_index] = 1; 
-                    if (write_ret != 0) {
-                        return write_ret;
-                    }
-                    c = 1;
-                    break;
-                }
-            }
-            if (c) {
-                continue;
-            }
-
-        }
-
-        int advance_ret = advance_offset_and_block(size, &offset, &block_no, file);
-        if (advance_ret == 2) {
-            return advance_ret;
-        }
-    }
-
-    if (list_arg_present) {
-        return print_list_arg_error(argv, print_file, list_arg_index, final_list_arg_index);
-    }
-
-    if (extract_arg_present) {
-
-        int fail = 0;
-        for (int i = extract_arg_index; i <= final_extract_arg_index; ++i) {
-            if (!print_file[i - extract_arg_index]) {
-                fflush(stdout);
-                fprintf(stderr, "mytar: %s: Not found in archive\n", argv[i]);
-                fail = 1;
-            }
-        }
-        if (fail) {
-            fflush(stdout);
-            fprintf(stderr, "mytar: Exiting with failure status due to previous errors\n");
-            return 2;
-        }
-        return 0;
-        
-    }
-}
-
 int write_to_file(FILE* file, FILE* create_file, int *offset, int *block_no, char size[]) {
     int size_len = 12;
     FILE *p = file;
@@ -546,4 +308,244 @@ int get_final_arg_index(int argc, char *argv[], int list_arg_index) {
         final_list_arg_index += 1;
     }
     return final_list_arg_index;
+}
+
+
+
+int main(int argc, char *argv[]) {
+
+    int list_arg_index = 0;
+    int file_arg_index = 0;
+    int extract_arg_index = 0;
+
+    int args_present[4] = {0, 0, 0, 0}; // Alphabetical order f -> t -> v -> x
+    
+    FILE* file = NULL;
+
+    int arg_parse_ret;
+    if ((arg_parse_ret = arg_parse(argc, argv, &list_arg_index, &file_arg_index, &extract_arg_index, args_present, &file)) != 0) {
+        return arg_parse_ret;
+    }
+    
+    int list_arg_present = check_list_arg_present(argc, argv, list_arg_index, file_arg_index) && args_present[1];
+    int extract_arg_present = check_list_arg_present(argc, argv, extract_arg_index, file_arg_index) && args_present[3];
+   
+    int final_list_arg_index = get_final_arg_index(argc, argv, list_arg_index);
+    int final_extract_arg_index = get_final_arg_index(argc, argv, extract_arg_index);
+
+    int pf_size = final_list_arg_index - list_arg_index + 1;
+    int print_file[pf_size];
+    initialise_with_zeros(print_file, pf_size);
+
+    int cr_size = final_extract_arg_index - extract_arg_index + 1;
+    int create_file[cr_size];
+    initialise_with_zeros(create_file, cr_size);
+
+    int offset = 0;
+    int block_no = 0;
+
+    int d;
+
+    char header[512];
+    char file_name[100];
+    char size[12];
+    char magic[6];
+    char typeflag;
+
+    while (file != NULL) {
+
+        // printf("shhs");
+
+        int pos = 0;
+        d = get_block(header, file, &pos);
+        block_no += 1;
+	// printf("%s\n", header);
+	// fflush(stdout);
+        
+        if (is_zero_block(header)) {
+            
+            FILE *p = file;
+            
+            for (int i = 0; i < 512; ++i) {
+                // Check partial block here?
+                int e;
+                if ((e = fgetc(p)) != '\0') {
+                    
+                    int this_ret = 0;
+                    if (list_arg_present) {
+                        this_ret = print_list_arg_error(argv, print_file, list_arg_index, final_list_arg_index);
+                    }
+
+                    printf("mytar: A lone zero block at %d\n", block_no); // ???
+		    fflush(stdout);
+                    return this_ret;
+                }
+            }
+
+            if (list_arg_present) {
+                return print_list_arg_error(argv, print_file, list_arg_index, final_list_arg_index);
+            }
+
+            return 0;
+        }
+
+        if (d == EOF) {
+            if (pos != 0) {
+                fflush(stdout);
+                fprintf(stderr, "mytar: Unexpected EOF in archive\n");
+                fflush(stdout);
+                fprintf(stderr, "mytar: Error is not recoverable: exiting now\n");
+                return 2;
+            }
+            if (pos == 0) {
+                return 0;
+            }
+        }
+        
+        for (int i = 0; i < 100; ++i) {
+            file_name[i] = header[i];
+        }
+        for (int i = 124; i < 136; ++i) {
+            size[i - 124] = header[i];
+        }
+        for (int i = 257; i < 263; ++i) {
+            magic[i - 257] = header[i];
+        }
+        typeflag = header[156];
+
+        if (magic[0] != 'u' || magic[1] != 's' || magic[2] != 't' || magic[3] != 'a' || magic[4] != 'r') {
+            fflush(stdout);
+            fprintf(stderr, "mytar: This does not look like a tar archive\n");
+            fflush(stdout);
+            fprintf(stderr, "mytar: Exiting with failure status due to previous errors\n");
+            return 2;
+        }
+
+        if (args_present[1] && typeflag != '0' && typeflag != '\0') {
+            fflush(stdout);
+            fprintf(stderr, "mytar: Unsupported header type: %d\n", typeflag);
+            return 2;
+        }
+
+        if (!list_arg_present && args_present[1]) {
+            print_default_list_output(file_name);
+        }
+
+        if (list_arg_present) {
+            print_list_arg_output(argv, print_file, file_name, list_arg_index, final_list_arg_index);
+        }
+
+        //fprintf(stderr, "%d\n", extract_arg_present);
+        //fprintf(stderr, "%d\n", args_present[3]);
+        //fprintf(stderr, "%d\n", args_present[2]);
+
+        if (!extract_arg_present && args_present[3] && !args_present[2]) { // Without -v
+            FILE* create_file = fopen(file_name, "w");
+            int write_ret = write_to_file(file, create_file, &offset, &block_no, size);
+            fclose(create_file);
+            if (write_ret != 0) {
+                return write_ret;
+            }
+            continue;
+        }
+        
+        // printf("%d\n", args_present[3]);
+        // printf("%d\n", args_present[2]);
+        if (!extract_arg_present && args_present[3] && args_present[2]) { // With -v
+            printf("%s\n", file_name);
+	    fflush(stdout);
+            //printf("shjhs\n");
+            FILE* create_file = fopen(file_name, "w");
+            int write_ret = write_to_file(file, create_file, &offset, &block_no, size);
+	    // printf("shhs\n");
+	    //fflush(stdout);
+            // fclose(create_file);
+	    // printf("%d\n", write_ret);
+	    // fflush(stdout);
+            if (write_ret != 0) {
+	    	// fflush(stdout);
+	    	// fprintf(stderr, "shhs\n");
+		//fflush(stdout);
+                return write_ret;
+            }
+	    //fflush(stdout);
+	    //fprintf(stderr, "shhs\n");
+	    
+            //printf("shs\n");
+            continue;
+        }
+
+        if (extract_arg_present && args_present[3] && !args_present[2]) { // Without -v
+
+            int c = 0;
+            for (int q = extract_arg_index; q <= final_extract_arg_index; q++) {
+                if (is_equal(argv[q], file_name) || is_prefix(argv[q], file_name) || is_suffix(argv[q], file_name)) {
+                    FILE* create_file = fopen(file_name, "w");
+                    int write_ret = write_to_file(file, create_file, &offset, &block_no, size);
+                    fclose(create_file);
+                    print_file[q - list_arg_index] = 1; 
+                    if (write_ret != 0) {
+                        return write_ret;
+                    }
+                    c = 1;
+                    break;
+                }
+            }
+            if (c) {
+                continue;
+            }
+            
+        }
+
+        if (extract_arg_present && args_present[3] && args_present[2]) {
+
+            int c = 0;
+            for (int q = extract_arg_index; q <= final_extract_arg_index; q++) {
+                if (is_equal(argv[q], file_name) || is_prefix(argv[q], file_name) || is_suffix(argv[q], file_name)) {
+                    printf("%s\n", file_name);
+                    FILE* create_file = fopen(file_name, "w");
+                    int write_ret = write_to_file(file, create_file, &offset, &block_no, size);
+                    fclose(create_file);
+                    print_file[q - list_arg_index] = 1; 
+                    if (write_ret != 0) {
+                        return write_ret;
+                    }
+                    c = 1;
+                    break;
+                }
+            }
+            if (c) {
+                continue;
+            }
+
+        }
+
+        int advance_ret = advance_offset_and_block(size, &offset, &block_no, file);
+        if (advance_ret == 2) {
+            return advance_ret;
+        }
+    }
+
+    if (list_arg_present) {
+        return print_list_arg_error(argv, print_file, list_arg_index, final_list_arg_index);
+    }
+
+    if (extract_arg_present) {
+
+        int fail = 0;
+        for (int i = extract_arg_index; i <= final_extract_arg_index; ++i) {
+            if (!print_file[i - extract_arg_index]) {
+                fflush(stdout);
+                fprintf(stderr, "mytar: %s: Not found in archive\n", argv[i]);
+                fail = 1;
+            }
+        }
+        if (fail) {
+            fflush(stdout);
+            fprintf(stderr, "mytar: Exiting with failure status due to previous errors\n");
+            return 2;
+        }
+        return 0;
+        
+    }
 }
