40419c55cec95dfb6580f38cf567eeeb651c2a25
Vishal Ramesh <vishal.ramesh@hotmail.com>
2020-08-20 19:51:05 +0200

    fread

--- mytar.c
+++ mytar.c
@@ -110,14 +110,13 @@ int advance_offset(char size[], int *offset, int *block_no, FILE* file) {
 char get_block(char header[], FILE *file, int *pos) {
     int d;
     int start = 0;
-    fread(header, 512, 1, file);
-    // while (start < 512 && (d = fgetc(file)) != EOF) {
-    //     header[start] = d;
-    //     start += 1;
-    // }
-    start = 512;
-    *pos = start;
     // fread(header, 512, 1, file);
+    while (start < 512 && (d = fgetc(file)) != EOF) {
+        header[start] = d;
+        start += 1;
+    }
+    // start = 512;
+    *pos = start;
     return d;
 }
 
