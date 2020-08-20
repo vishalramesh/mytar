acdf96717686cb96fc310860f0395bd25f2916a0
Vishal Ramesh <vishal.ramesh@hotmail.com>
2020-08-20 21:06:26 +0200

    test

--- mytar.c
+++ mytar.c
@@ -118,6 +118,8 @@ char get_block(char header[], FILE *file, int *pos) {
     // *pos = start;
     // start = 512;
     d = header[start - 1];
+    fprintf(stderr, "%d\n", start);
+    fprintf(stderr, "%d\n", pos);
     if (d == EOF) {
         *pos = start - 1;
     } else {
