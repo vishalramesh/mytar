fdeb9da8416ab86fdc70569821995fcf4c479005
Vishal Ramesh <vishal.ramesh@hotmail.com>
2020-08-20 21:02:19 +0200

    test

--- mytar.c
+++ mytar.c
@@ -110,19 +110,19 @@ int advance_offset(char size[], int *offset, int *block_no, FILE* file) {
 char get_block(char header[], FILE *file, int *pos) {
     int d;
     int start = 0;
-    //start = fread(header, 512, 1, file);
-    while (start < 512 && (d = fgetc(file)) != EOF) {
-        header[start] = d;
-        start += 1;
-    }
-    *pos = start;
-    // start = 512;
-    // d = header[start - 1];
-    // if (d == EOF) {
-    //     *pos = start - 1;
-    // } else {
-    //     *pos = start;
+    start = fread(header, 512, 1, file);
+    // while (start < 512 && (d = fgetc(file)) != EOF) {
+    //     header[start] = d;
+    //     start += 1;
     // }
+    // *pos = start;
+    // start = 512;
+    d = header[start - 1];
+    if (d == EOF) {
+        *pos = start - 1;
+    } else {
+        *pos = start;
+    }
     return d;
 }
 
@@ -315,7 +315,7 @@ int main(int argc, char *argv[]) {
 
         int pos = 0;
         d = get_block(header, file, &pos);
-        // fseek(file, offset + 512, SEEK_SET);
+        fseek(file, offset + 512, SEEK_SET);
         block_no += 1;
 
         if (is_zero_block(header)) {
